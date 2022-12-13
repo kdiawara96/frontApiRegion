c262917da808489ef078b1a2201b79dc320623b1
kdiawara96 <karimdiawara96@gmail.com>
2022-12-13 14:16:30 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frontApi',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
